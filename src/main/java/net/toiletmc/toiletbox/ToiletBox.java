ef88c05a3c4f3e23d503b3bf8ed45183b3bcde16
TheLittle_Yang <2570984321@qq.com>
2023-08-07 22:26:12 +0800

    init

---
+++ src/main/java/net/toiletmc/toiletbox/ToiletBox.java
@@ -0,0 +1,17 @@
+package net.toiletmc.toiletbox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ToiletBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
